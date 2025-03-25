aea07f1a404d7ac3f7c9965840b12c36700d55f5
Digestey <lukas.waller36@gmail.com>
2025-03-25 13:01:19 +0100

    The idea now is to make a pokemon guessing game with a high score table locked behind a form. Added some sample code for that.

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,6 +1,19 @@
-function test(): void {
-    console.log(process.env.TEST_VALUE);
-    console.log("World")
-  }
-  
-test();
+import { MainClient } from 'pokenode-ts';
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+const api = new MainClient();
+const MAX_POKEMON = 1025;
+
+app.get("/", async (req, res) => {
+  var pokemon:string = "";
+  const pokemon_id:number = Math.round(Math.random() * MAX_POKEMON);
+  await api.pokemon
+  .getPokemonById(pokemon_id)
+  .then((data) => pokemon = data.name) // will output "Luxray"
+  .catch((error) => console.error(error));
+  res.send("My favourite pokemon is "+pokemon);
+});
+
+app.listen(PORT);
