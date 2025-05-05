e1faccda5c14d9ff84f4ca2ef5491040781c3698
Digestey <lukas.waller36@gmail.com>
2025-05-05 23:21:31 +0200

    added my project files to rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -10,13 +13,18 @@ project = 'DexQuiz'
 copyright = '2025, Lukas Waller'
 author = 'Lukas Waller'
 
+sys.path.insert(0, os.path.abspath('../project/app'))
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
 
 
 
